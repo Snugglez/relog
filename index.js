8b2e6de40c8ffd075f4f4dd31e1d40c8371e7300
valkyr1e <nkpoidjp+tera@gmail.com>
2018-12-08 04:03:16 +0900

    refine

--- index.js
+++ index.js
@@ -1,102 +1,75 @@
-const Command = require('command')
+module.exports = function Relog(mod) {
+  let characters = []
+  let charIndex = -1
 
-module.exports = function Relog(dispatch) {
-  const command = Command(dispatch)
+  mod.command.add('relog', arg => {
+    if (arg === 'nx') {
+      if (++charIndex === characters.length)
+        charIndex = 0
+    } else if (/^\d+$/.test(arg)) {
+      const nextCharIndex = Number(arg)
+      if (nextCharIndex >= characters.length) {
+        mod.command.message(`Not found ${nextCharIndex}th character`)
+        return
+      } else {
+        charIndex = nextCharIndex - 1
+      }
+    } else {
+      let nextCharIndex = -1
 
-  let positions = {},
-      curr_char = -1
+      for (let i=0; i<characters.length; i++) {
+        if (characters[i].name.toLowerCase() === arg.toLowerCase()) {
+          nextCharIndex = i
+          break
+        }
+      }
 
-  command.add('relog', (name) => {
-    if (!name) return
-    getCharacterId(name)
-      .then(relog)
-      .catch(e => console.error(e.message))
+      if (nextCharIndex < 0) {
+        mod.command.message(`Not found '${arg}'`)
+        return
+      } else {
+        charIndex = nextCharIndex
+      }
+    }
+
+    relog(characters[charIndex].id)
   })
 
   // Grab the user list the first time the client sees the lobby
-  dispatch.hook('S_GET_USER_LIST', 14, event => updatePositions(event.characters))
-
-  dispatch.hook('C_DELETE_USER', 'raw', () =>
-    dispatch.hookOnce('S_GET_USER_LIST', 14, event => updatePositions(event.characters))
-  )
-
-  // Update positions on reorder
-  dispatch.hook('C_CHANGE_USER_LOBBY_SLOT_ID', 1, event => {
-    updatePositions(event.characters)
-    console.log('[relog] Character positions updated')
+  mod.hook('S_GET_USER_LIST', 14, event => {
+    characters = event.characters
   })
 
   // Keep track of current char for relog nx
-  dispatch.hook('C_SELECT_USER', 1, /*{order: 100, filter: {fake: null}},*/ event => {
-    curr_char = positions[event.id]
-    console.log('[relog] Char selected: ' + curr_char)
+  mod.hook('C_SELECT_USER', 1, event => {
+    for (let i=0; i<characters.length; i++) {
+      if (characters[i].id === event.id) {
+        charIndex = i
+        break
+      }
+    }
   })
-
-  function send(msg) {
-    command.message(' (relog): ' + msg)
-  }
-
-  function updatePositions(characters) {
-    if (!characters) return
-    positions = {}
-    characters.forEach((char, i) => {
-      let {id, position} = char
-      positions[id] = position || (i+1)
-    })
-  }
-
-  function getCharacterId(name) {
-    return new Promise((resolve, reject) => {
-      // request handler, resolves with character's playerId
-      const userListHook = dispatch.hookOnce('S_GET_USER_LIST', 14, event => {
-        name = name.toLowerCase()
-        let index = (name === 'nx')? ++curr_char : parseInt(name)
-        if (index && index > event.characters.length) index = 1
-        event.characters.forEach((char, i) => {
-          if (char.deletion) return
-          let pos = char.position || (i+1)
-          if (char.name.toLowerCase() === name || pos === index) resolve(char.id)
-        })
-        reject(new Error(`[relog] character "${name}" not found`))
-      })
-
-      // set a timeout for the request, in case something went wrong
-      setTimeout(() => {
-        if (userListHook) dispatch.unhook(userListHook)
-        reject(new Error('[relog] C_GET_USER_LIST request timed out'))
-      }, 6000)
-
-      // request the character list
-      dispatch.toServer('C_GET_USER_LIST', 1, {})
-    })
-  }
-
-  function relog(targetId) {
-    if (!targetId) return
-    dispatch.toServer('C_RETURN_TO_LOBBY', 1, {})
-    let userListHook
+  
+  function relog(id) {
+    mod.toServer('C_RETURN_TO_LOBBY', 1, {})
     let lobbyHook
 
     // make sure that the client is able to log out
-    const prepareLobbyHook = dispatch.hookOnce('S_PREPARE_RETURN_TO_LOBBY', 1, () => {
-      dispatch.toClient('S_RETURN_TO_LOBBY', 1, {})
-
-      // the server is not ready yet, displaying "Loading..." as char names
-      userListHook = dispatch.hookOnce('S_GET_USER_LIST', 14, event => {
-        event.characters.forEach(char => char.name = 'Loading...')
-        return true
-      })
+    const prepareLobbyHook = mod.hookOnce('S_PREPARE_RETURN_TO_LOBBY', 1, () => {
+      mod.toClient('S_RETURN_TO_LOBBY', 1, {})
 
       // the server is ready to relog to a new character
-      lobbyHook = dispatch.hookOnce('S_RETURN_TO_LOBBY', 1, () => {
-        process.nextTick (() => dispatch.toServer('C_SELECT_USER', 1, { id: targetId, unk: 0 }))
+      lobbyHook = mod.hookOnce('S_RETURN_TO_LOBBY', 1, () => {
+        process.nextTick(() => mod.toServer('C_SELECT_USER', 1, {id: id,unk: 0}))
       })
     })
 
     // hook timeout, in case something goes wrong
     setTimeout(() => {
-      for (const hook of [prepareLobbyHook, lobbyHook, userListHook])
-        if (hook) dispatch.unhook(hook)
+      for (const hook of [prepareLobbyHook, lobbyHook]) {
+        if (hook)
+          mod.unhook(hook)
+      }
     }, 16000)
   }
 }
